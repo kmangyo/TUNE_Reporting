17005fb3993932c18badb0aec4b90aa21fd9ecbf
Hyun Gyo, Kim <khg423@gmail.com>
2016-05-12 17:43:54 +0900

    Update server.R

--- tune_shiny_realtime/server.R
+++ tune_shiny_realtime/server.R
@@ -34,19 +34,18 @@ shinyServer(function(input, output, session) {
       } else {
           while(dl_url$data$status != c('complete')){
           dl_url<-fromJSON(file=csv_url)
-          print(dl_url$data$percent_complete)
           if(dl_url$data$status == c('complete')){
           return(dl_url)
         }
     }  
-#         while(is.null(dl_url$data)){
-#         dl_url<-fromJSON(file=csv_url)
-#         print(dl_url$erros)
-#            if(!is.null(dl_url$data)){
-#            dl_url <- dl_url$data$data$url
-#            return(dl_url)
-#          }
-#        }  
+          #while(is.null(dl_url$data)){
+          #dl_url<-fromJSON(file=csv_url)
+          #print(dl_url$erros)
+          #  if(!is.null(dl_url$data)){
+          #  dl_url <- dl_url$data$data$url
+          #  return(dl_url)
+          #}
+        #}  
       }
     }
   })
@@ -61,7 +60,31 @@ shinyServer(function(input, output, session) {
   })
 
   
-  output$report <- DT::renderDataTable(DT::datatable({
+  output$report_time <- DT::renderDataTable(DT::datatable({
+    if(input$dl_url==c('')){
+      report<-data.frame()
+    } else {
+    report<-read.csv(input$dl_url)
+    report$site_event.name<-toupper(report$site_event.name)
+    report<-report %>% group_by(timestamp, publisher.name, advertiser_sub_ad.name, site_event.name) %>% summarise(ad_click=sum(ad_clicks),installs=sum(installs), revenues=sum(revenues),events=sum(events) ,opens=sum(opens))
+    report<-data.frame(report)
+    report<-reshape(report, idvar=c("timestamp","publisher.name","advertiser_sub_ad.name"), timevar="site_event.name", direction="wide")
+    report[4:ncol(report)][is.na(report[4:ncol(report)])]<-0
+    report<-report[c('timestamp','publisher.name','advertiser_sub_ad.name','ad_click.','installs.INSTALL','revenues.PURCHASE','events.PURCHASE','events.REGISTRATION','opens.OPEN')]
+    report <- subset(report, publisher.name!=c('organic'))    
+    report$CVR.install<-with(report, (installs.INSTALL/ad_click.))
+    report$CVR.install[is.na(report$CVR.install)]<-0
+    report$CVR.install[is.infinite(report$CVR.install)]<-0
+    report$CVR.install<-percent(report$CVR.install)
+    names(report)<-c('Timestamp','Publisher.name','Advertiser_sub_ad.name','Ad_click','Install','Revenues','Purchase','Registration','Open','CVR.install')
+    report<-report[c(1:5,10,6:9)]
+    report$id<-with(report,paste0(Publisher.name,c('_'),Advertiser_sub_ad.name))
+    report<- report %>% arrange(id)
+    report
+    }
+  }))
+
+  output$report_cumsum <- DT::renderDataTable(DT::datatable({
     if(input$dl_url==c('')){
       report<-data.frame()
     } else {
@@ -73,11 +96,12 @@ shinyServer(function(input, output, session) {
     report[4:ncol(report)][is.na(report[4:ncol(report)])]<-0
     report<-report[c('timestamp','publisher.name','advertiser_sub_ad.name','ad_click.','installs.INSTALL','revenues.PURCHASE','events.PURCHASE','events.REGISTRATION','opens.OPEN')]
     report <- subset(report, publisher.name!=c('organic'))
-    
-    report$CVR.install<-percent(with(report, (installs.INSTALL/ad_click.)))
+    report$CVR.install<-with(report, (installs.INSTALL/ad_click.))
+    report$CVR.install[is.na(report$CVR.install)]<-0
+    report$CVR.install[is.infinite(report$CVR.install)]<-0
+    report$CVR.install<-percent(report$CVR.install)
     names(report)<-c('Timestamp','Publisher.name','Advertiser_sub_ad.name','Ad_click','Install','Revenues','Purchase','Registration','Open','CVR.install')
     report<-report[c(1:5,10,6:9)]
-
     report$id<-with(report,paste0(Publisher.name,c('_'),Advertiser_sub_ad.name))
     report<- report %>% arrange(id, Timestamp)
     report$cumsum_ad_click<-with(report, ave(Ad_click,id,FUN=cumsum))
@@ -87,9 +111,10 @@ shinyServer(function(input, output, session) {
     report$cumsum_PURCHASE<-with(report, ave(Purchase,id,FUN=cumsum))
     report$cumsum_REGISTRATION<-with(report, ave(Registration,id,FUN=cumsum))
     report$cumsum_OPEN<-with(report, ave(Open,id,FUN=cumsum))
+    report<-report[c('Timestamp','Publisher.name','Advertiser_sub_ad.name','cumsum_ad_click','cumsum_install','cumsum.CVR.install','cumsum_revenues','cumsum_PURCHASE','cumsum_REGISTRATION','cumsum_OPEN','id')]
+    report<- report %>% arrange(id)
     report
     }
   }))
 
 })
-
