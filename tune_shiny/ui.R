9c36bb3683d1663b98490563e560120f0910e5a2
Hyun Gyo, Kim <khg423@gmail.com>
2016-05-24 16:37:49 +0900

    Update ui.R

--- tune_shiny/ui.R
+++ tune_shiny/ui.R
@@ -8,17 +8,11 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel( textInput("api", label = h3("API Key"), value = ""),
                   dateRangeInput("dates", label = h3("Date range")),submitButton("Update View"),
-                  textInput("job_id", label = h3("Job_ID"), value = ""),
-                  textInput("job_id_puv", label = h3("Job_ID_PUV"), value = ""), submitButton("Update View"),
                   textInput("dl_url", label = h3("DL_URL"), value = ""), submitButton("Update View"),
                   textInput("dl_url_puv", label = h3("DL_URL_PUV"), value = ""), submitButton("Update View")
                   ),
 
     mainPanel(
-      h4("Job_ID"),
-      verbatimTextOutput("job_id"),
-      h4("Job_ID_PUV"),
-      verbatimTextOutput("job_id_puv"),      
       h4("DL_URL"),         
       verbatimTextOutput("dl_url"),
       h4("DL_URL_PUV"),         
